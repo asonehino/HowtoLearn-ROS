c1d746df34be597439fe2a59129e91d576461b65
hyeonho <asonehino@naver.com>
2025-03-18 09:46:23 +0900

    server and client for robot

---
+++ 01_Session/client.py
@@ -0,0 +1,29 @@
+import os
+import time
+import socket
+import threading
+from pynput import keyboard
+
+#소켓 옵젝 만들기
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#서버 연결
+host = 'localhost'
+port = 12345
+client_socket.connect((host, port))
+print('서버에 연결')
+flagClient = True
+while flagClient:
+  ch = input('입력: ')
+  client_socket.send(ch.encode())
+
+  response = client_socket.recv(1024)
+  respData = response.decode()
+  print('에코: ', respData)
+
+  if (ch == 'q'):
+    break
+
+client_socket.sendall('quit'.encode())
+client_socket.close()
+print('끝')
---
+++ 01_Session/server.py
@@ -0,0 +1,33 @@
+import socket
+
+#binding
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = 'localhost'
+port = 12345
+server_socket.bind((host, port))
+
+#듣기 모드 대기
+server_socket.listen(1)
+print('서버 시작, 기다려려')
+
+flagServer = True
+while flagServer:
+  #클라이언트 연결 시도 대기
+  client_socket, address = server_socket.accept()
+  print('연결되었다', address)
+
+  while True:
+    data = client_socket.recv(1024)
+    if not data:
+      break
+
+    rcvData = data.decode()
+    if not (rcvData == 'quit'):
+      flagServer = False
+      print('수신: ', rcvData.encode())
+    else:
+      print('종료: ', rcvData.encode())
+      break
+    client_socket.sendall(data)
+    
+client_socket.close()
